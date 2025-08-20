c24bc39b9c4c1fe21c722390441479f47645c475
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2025-08-20 10:36:41 +0200

    update main for visualizing UI

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,1 +1,17 @@
 package src.main.java;
+
+import java.sql.Connection;
+
+import src.main.java.db.ConnectionDB;
+import src.main.java.ui.concreteFrames.Welcome;
+
+class main{
+    public static void main(String...args){
+        ConnectionDB db = new ConnectionDB();
+        Connection connessione = db.getConnection();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            ConnectionDB.getIstance().closeConnection();
+        }));
+        Welcome welcomePage = new Welcome();
+    }
+}
