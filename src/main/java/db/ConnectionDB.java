d52a600a50bc4e448ba03088cebcd5bb15627c5b
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2025-08-20 10:28:30 +0200

    Aggiunta gestione connessione al DB MySQL

---
+++ src/main/java/db/ConnectionDB.java
@@ -0,0 +1,52 @@
+package src.main.java.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private static ConnectionDB istance;
+    private Connection connection;
+
+    private String url;
+    private String user;
+    private String pwd;
+
+    public ConnectionDB(){
+        initializeDBCredentials();
+        try{
+            connection = DriverManager.getConnection(url, user, pwd);
+            connection.setAutoCommit(true);
+        }catch(SQLException e){
+            System.out.println("Si è verificato un errore durante la connessione.");
+        }
+    }
+
+    private void initializeDBCredentials(){
+        url = "jdbc:mysql://localhost:3306/libreria";
+        user = "root";
+        pwd = "root";
+    }
+
+    public static synchronized ConnectionDB getIstance(){
+        if(istance == null){
+            return new ConnectionDB();
+        }
+        return istance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public void closeConnection(){
+        try{
+            if(connection != null){
+                connection.close();
+            }
+        }catch(SQLException e){
+            System.out.println("Errore nella chiusura della connessione con il DB:");
+        }
+    }
+    
+}
