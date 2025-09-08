cd450b58afbdfcb80ed79936ac42d9fcd665e3be
Giuseppe De Marco <g.demarco@eurolink.it>
2025-09-08 15:32:32 +0200

    rimozione numIst per System.out.println()

--- src/main/java/commands/AbstractCommand.java
+++ src/main/java/commands/AbstractCommand.java
@@ -6,18 +6,10 @@ import java.sql.SQLException;
 import src.main.java.db.ConnectionDB;
 
 public abstract class AbstractCommand implements Command{
-    private static int count = 1;
-    private final int numeroIst;
     private String query;
 
     public AbstractCommand(String q){
-        this.numeroIst = count;
         this.query = q;
-        count++;
-    }
-
-    public String toString(){
-        return numeroIst + "-"+query;
     }
 
     public PreparedStatement createStatement() throws SQLException {
