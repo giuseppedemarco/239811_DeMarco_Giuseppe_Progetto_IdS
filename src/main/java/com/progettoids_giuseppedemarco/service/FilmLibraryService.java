0dc7742940d30b3ff26991e2fb53d9eeb473f043
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-19 00:26:22 +0100

    Added FilmLibraryService for filmRepository

---
+++ src/main/java/com/progettoids_giuseppedemarco/service/FilmLibraryService.java
@@ -0,0 +1,52 @@
+package com.progettoids_giuseppedemarco.service;
+
+import com.progettoids_giuseppedemarco.builder.FilmBuilder;
+import com.progettoids_giuseppedemarco.domain.Film;
+import com.progettoids_giuseppedemarco.repository.FilmRepository;
+import com.progettoids_giuseppedemarco.specification.FilmSpecification;
+import com.progettoids_giuseppedemarco.strategy.FilmSortContext;
+import com.progettoids_giuseppedemarco.strategy.FilmSortStrategy;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class FilmLibraryService {
+    private final FilmRepository filmRepository;
+
+    public FilmLibraryService(FilmRepository filmRepository) {
+        this.filmRepository = filmRepository;
+    }
+
+    public Film aggiungiFilm(FilmBuilder builder) {
+        Film film = builder.getResult();
+        return filmRepository.save(film);
+    }
+
+    public Film aggiornaFilm(int id, FilmBuilder builder) {
+        filmRepository.findById(id).orElseThrow(() -> new NoSuchElementException("film non trovato: " + id));
+        builder.setId(id);
+        Film filmAggiornato = builder.getResult();
+        return filmRepository.save(filmAggiornato);
+    }
+
+    public void rimuoviFilm(int id) {
+        filmRepository.deleteById(id);
+    }
+
+    public List<Film> listaFilm() {
+        return filmRepository.findAll();
+    }
+
+    public List<Film> cercaPerSpecifica(FilmSpecification specification) {
+        return filmRepository.findAll()
+                .stream()
+                .filter(specification::isSatisfiedBy)
+                .collect(Collectors.toList());
+    }
+
+    public List<Film> ordina(FilmSortStrategy strategy) {
+        FilmSortContext context = new FilmSortContext(strategy);
+        return context.executeStrategy(filmRepository.findAll());
+    }
+}
