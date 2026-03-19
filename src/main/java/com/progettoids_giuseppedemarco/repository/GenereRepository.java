9c8667e57c3f9f51713d28e42c0e87ec42aa64b8
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-19 01:40:22 +0100

    Removed Genere and Regista Repo due to handling films in FilmRepo

--- src/main/java/com/progettoids_giuseppedemarco/repository/GenereRepository.java
+++
@@ -1,7 +0,0 @@
-package com.progettoids_giuseppedemarco.repository;
-
-import com.progettoids_giuseppedemarco.domain.Genere;
-
-public interface GenereRepository extends CrudRepository<Genere, Integer> {
-}
-
--- src/main/java/com/progettoids_giuseppedemarco/repository/RegistaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.progettoids_giuseppedemarco.repository;
-
-import com.progettoids_giuseppedemarco.domain.Regista;
-
-public interface RegistaRepository extends CrudRepository<Regista, Integer> {
-}
-
