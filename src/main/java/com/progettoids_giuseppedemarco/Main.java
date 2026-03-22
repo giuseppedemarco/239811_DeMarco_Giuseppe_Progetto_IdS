90fb8900236884ecdf6bd274b89a68a717eec41a
Giuseppe De Marco <giuseppedemarco2004@virgilio.it>
2026-03-22 09:54:18 +0100

    Added main.java

--- src/main/java/com/progettoids_giuseppedemarco/Main.java
+++ src/main/java/com/progettoids_giuseppedemarco/Main.java
@@ -1,7 +1,9 @@
 package com.progettoids_giuseppedemarco;
 
+import com.progettoids_giuseppedemarco.ui.FilmLibraryApp;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FilmLibraryApp.launchApp(args);
     }
 }
